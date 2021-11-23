d3a731462e739a5f9f00a02618fb019aa33faa49
Cherod Joseph <jcherod365@gmail.com>
2021-11-23 21:11:29 +0100

    esquisse prog

--- main.py
+++
@@ -1,8 +0,0 @@
-"""
-from utils2 import get_categories_url
-
-
-# Retrieve all categories url
-category_urls =  get_categories_url()
-
-"""
--- utils.py
+++ utils.py
@@ -32,9 +32,10 @@ class Book:
             url1 = "https://books.toscrape.com/catalogue/category/books/" + str(t) + "_" + str(self.i) + "/index.html"
             self.listUrlCategorie.append(url1)
 
-category_urls = Book('https://books.toscrape.com/index.html', requests.get(url),
+category_url  = Book('https://books.toscrape.com/index.html', requests.get(url),
                      BeautifulSoup(page.content, 'html.parser',
                     soup.find('aside', {'class': 'sidebar col-sm-4 col-md-3'}).find('ul').findAll('li'),
-                    [], 0, 1))
-category_urls = category_urls.get_categories_url()
+                    [], 0, 1)
+
+category_url.get_categories_url()
 print(category_urls)
