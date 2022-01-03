f0a6754637114d0c15b28962d3a5796d5494f5eb
Cherod Joseph <jcherod365@gmail.com>
2022-01-03 22:34:23 +0100

    creation dico/liste

--- views/menu.py
+++ views/menu.py
@@ -34,30 +34,36 @@ class PlayerView:
             sys.exit("you must enter an integer")
 
         for counts in range(numberplayers):
-
+            i=0
+            player = "player"
             print()
             print("the player number {}".format(counts + 1))
             print()
-            lastname = ""
-            while lastname == "":
-                lastname = input("lastname? : ")
 
-            firstname = ""
-            while firstname == "":
-                firstname = input("firstname? : ")
-
+            lastname = input("lastname? : ")
+            firstname = input("firstname? : ")
             birth_date = input("birth_date? : ")
-
-            sex = ""
-            while sex != "m" or sex !="f":
-                sex = input("sex m/f? : ")
-
+            sex = input("sex m/f? : ")
             age = input("age? : ")
-            try:
-                age = int(age)
-            except ValueError:
-                sys.exit("you must enter an integer")
             rank = input("rank /10? : ")
+            print()
+            print("player identity {}".format(counts + 1))
+            print()
+
+
+        def createList(name, n):
+            result = {}
+            for i in range(n):
+                nameList = name + str(i + 1)
+                result[nameList] = []
+            return result
+
+        res = createList("liste", 5)  # création de 5 listes dont le nom commence par list
+
+        for i in range(5):
+            res["liste" + str(i + 1)].extend([2, 4])  # on ajoute 2 à list3
+
+        print(res)  # {'list1': [], 'list5': [], 'list2': [], 'list4': [], 'list3': [2]}
 
 
 class TournamentView:
