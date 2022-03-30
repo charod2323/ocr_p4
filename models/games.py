7093fc1a11620865a41d90cb54aff3984da0b71d
Cherod Joseph <jcherod365@gmail.com>
2022-03-30 05:40:31 +0200

    recuperer elements

--- models/games.py
+++ models/games.py
@@ -1,6 +1,60 @@
+from tinydb import TinyDB, Query
+
 class Game:
-    def __init__(self, player1, player2):
+    def __init__(self, player1, player2,score, number_games):
+        self.number_games = number_games
         self.player1 = player1
         self.player2 = player2
-        self.score = None
-        self.list_players_games = None
+        self.score = score
+
+
+    def get_serialized_player(self):
+        """
+        Return tinydb serialized player.
+        :return:
+        """
+        return {
+            "number_games": self.number_games,
+            "player1": self.player1,
+            "player2": self.player2,
+            "score": self.score
+             }
+
+    def save_to_tiny_db(self):
+        """
+
+        :return:
+        """
+        db = TinyDB("db.json")
+        games_table = db.table("games")
+        games_table.insert(self.get_serialized_player())
+
+    @classmethod
+    def get_all_players_from_db(cls):
+        db = TinyDB("db.json")
+        games_table = db.table("games")
+        return games_table.all()
+
+    @classmethod
+    def get_games_by_number_games(cls, number_games):
+        """
+
+        :param number_games:
+        :return:
+        """
+        q = Query()
+        db = TinyDB("db.json")
+        print(db)
+        print("number_games:", number_games)
+        return db.search(q.games.number_games == number_games)
+
+    @classmethod
+    def save_players(cls, serialized_games):
+        """
+
+        :param serialized_games:
+        :return:
+        """
+        db = TinyDB("db.json")
+        games_table = db.table("games")
+        games_table.insert_multiple(serialized_games)
--- models/rounds.py
+++ models/rounds.py
@@ -1,10 +1,65 @@
+from tinydb import TinyDB, Query
+
+
+
 class Round:
 
-    def __init__(self, lastname, games, date, heure_debut, date2, heure_fin):
+    def __init__(self, round_number, games, date, heure_debut, heure_fin):
 
-        self.lastname = lastname
+        self.round_number = round_number
         self.date = date
         self.heure_debut = heure_debut
         self.heure_fin = heure_fin
-        self.date2 = date2
         self.games = games
+
+    def get_serialized_player(self):
+        """
+        Return tinydb serialized player.
+        :return:
+        """
+        return {
+            "round_number": self.round_number,
+            "date": self.date,
+            "heure_debut": self.heure_debut,
+            "heure_fin": self.heure_fin,
+            "games": self.games
+             }
+
+    def save_to_tiny_db(self):
+        """
+
+        :return:
+        """
+        db = TinyDB("db.json")
+        rounds_table = db.table("rounds")
+        rounds_table.insert(self.get_serialized_player())
+
+    @classmethod
+    def get_all_rounds_from_db(cls):
+        db = TinyDB("db.json")
+        rounds_table = db.table("rounds")
+        return rounds_table.all()
+
+    @classmethod
+    def get_player_by_round_number(cls, round_number):
+        """
+
+        :param round_number:
+        :return:
+        """
+        q = Query()
+        db = TinyDB("db.json")
+        print(db)
+        print("round_number:", round_number)
+        return db.search(q.rounds.round_number == round_number)
+
+    @classmethod
+    def save_players(cls, serialized_rounds):
+        """
+
+        :param serialized_rounds:
+        :return:
+        """
+        db = TinyDB("db.json")
+        rounds_table = db.table("rounds")
+        rounds_table.insert_multiple(serialized_rounds)
